16629fbde16ee5666f60f4b3296e0ea53f0e05bf
Felipe Tertuliano <felipeltertuliano@gmail.com>
2026-07-14 18:44:48 -0300

    chore:  PCA basic structure

--- src/data/algorithms/pca.rs
+++ src/data/algorithms/pca.rs
@@ -1,3 +1,38 @@
 use super::super::DataSource;
+use crate::types::GlobalRes;
 
-impl DataSource {}
+impl DataSource {
+    //TODO: remove underscore
+    pub async fn _pca<FE>(&mut self, to: Option<&str>, exclude: Option<FE>) -> GlobalRes<Self>
+    where
+        FE: Fn(&str) -> bool,
+    {
+        let mut reduced = self.child(to)?;
+        if !reduced.exists() {
+            self.read(true, None)?;
+            //TODO: remove underscore
+            let _headers = if let Some(exclude_fn) = exclude {
+                self.get_header()?.iter().fold(vec![], |mut acc, (h, _)| {
+                    if !exclude_fn(h) {
+                        acc.push(h);
+                    }
+                    acc
+                })
+            } else {
+                self.get_header()?.keys().collect()
+            };
+            self.read(false, None)?;
+            reduced.init().await?;
+            reduced.write(true)?;
+            self.foreach(|_di| {
+                //TODO: implement PCA
+                Ok(())
+            })
+            .await?;
+            reduced.write(false)?;
+        } else {
+            reduced.init().await?;
+        }
+        Ok(reduced)
+    }
+}
