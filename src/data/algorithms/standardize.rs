6ef3790181df0379983627da6c8286a1242f603f
Felipe Tertuliano <felipeltertuliano@gmail.com>
2026-08-17 14:29:05 -0300

    feat: standardize

--- src/data/algorithms/standardize.rs
+++ src/data/algorithms/standardize.rs
@@ -1,17 +1,14 @@
 use super::super::DataSource;
-use crate::types::{GlobalRes, Number};
+use crate::types::GlobalRes;
 
 impl DataSource {
-    pub async fn standardize(
-        &mut self,
-        to: Option<&str>,
-        include: &Vec<&str>,
-    ) -> GlobalRes<Self> {
-        let mut standarized = self.child(to)?;
-        if !standarized.exists() {
+    pub async fn standardize(&mut self, to: Option<&str>, include: &Vec<&str>) -> GlobalRes<Self> {
+        let mut standardized = self.child(to)?;
+        if !standardized.exists() {
+            let mut variances: Vec<(&str, f64)> =
+                include.clone().iter().map(|x| (*x, 0.0)).collect();
+            let mut means = variances.clone();
             let mut n: u32 = 0;
-            let mut means: Vec<(&str, f64)> = include.clone().iter().map(|x| (*x, 0.0)).collect();
-            let mut variances = means.clone();
             self.foreach(|di| {
                 n += 1;
                 for (header, value) in &mut means {
@@ -34,10 +31,27 @@ impl DataSource {
             })
             .await?;
             for (_, value) in &mut variances {
-                *value = value.sqrt()
+                *value = value.sqrt();
             }
-            standarized.init().await?;
+            standardized.init().await?;
+            standardized.write(true)?;
+            self.foreach(|mut di| {
+                if let Some(all_headers) = di.get_header() {
+                    standardized.set_header(all_headers)?;
+                    for i in 0..include.len() {
+                        let (header, variance) = &variances[i];
+                        let (_, mean) = &means[i];
+                        di.set(header, (di.get::<f64>(header).unwrap() - mean) / variance);
+                    }
+                    standardized.write_line(di.to_string())?;
+                }
+                Ok(())
+            })
+            .await?;
+            standardized.write(false)?;
+        } else {
+            standardized.init().await?;
         }
-        Ok(standarized)
+        Ok(standardized)
     }
 }
